f2d5ba19dc81362d8b9818a68ae59066c5a06527
Bruno Barratella <brubarratella@hotmail.com>
2023-02-19 19:06:18 -0300

    Consulta de Dados no localStorage

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) =>{
+    console.log(elemento.nome, elemento.quantidade)
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-const nome = evento.target.elements['nome']
-const quantidade = evento.target.elements['quantidade']
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
     criaElemento(nome.value, quantidade.value)
 
@@ -14,17 +19,23 @@ const quantidade = evento.target.elements['quantidade']
 })
 
 function criaElemento (nome, quantidade){
-    const novoItem = document.createElement ('li')
+    const novoItem = document.createElement ("li")
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement ('strong')
+    const numeroItem = document.createElement ("strong")
     numeroItem.innerHTML = quantidade
-
     novoItem.appendChild(numeroItem)
+
     novoItem.innerHTML += nome
     
     lista.appendChild(novoItem)
 
-    localStorage.setItem("nome", nome)
-    localStorage.setItem("quantidade", quantidade)
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
